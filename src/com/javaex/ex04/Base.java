7c20e056c5a1667dfaa4cd9e689c9d94e0693057
최정필 <wjdvlf1407@gmail.com>
2022-04-22 23:30:20 +0900

    Ex04 인스턴스 초기화

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -1,10 +1,33 @@
 package com.javaex.ex04;
 
+
 public class Base {
-    
+	
+	//필드
+	private String service;	
+	
+	//생성자
+	
+    //메소드 - gs
+	
+	
+	//메소드 - 일반
+	
+	
     public void service(String state) {
-        
-        //코드작성할것
+    	//코드작성할 것
+        this.service=state;
+        if (service.equals("낮")) {		//인스턴스 초기화
+        	day();
+			
+		} else if(service.equals("밤")) {
+			night();
+
+		} else {
+			afternoon();
+			
+		}
+			
     }
 
     public void day() {
@@ -12,12 +35,13 @@ public class Base {
     }
 
     public void night() {
-        
     	//코드작성할것
+    	System.out.println("밤에는 숙면");
     }
     
     public void afternoon(){
         System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
     }
+    
 }
 
