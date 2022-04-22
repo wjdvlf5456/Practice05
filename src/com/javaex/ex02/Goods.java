9cee4a7303d0fca940f1d93606a35d9dad9cc93c
최정필 <wjdvlf1407@gmail.com>
2022-04-22 19:25:57 +0900

    Ex02 기본틀

--- src/com/javaex/ex02/Goods.java
+++ src/com/javaex/ex02/Goods.java
@@ -1,7 +1,20 @@
 package com.javaex.ex02;
 
 public class Goods {
-
+	
+	//필드
+	private String name;
+	private int price;
+	
+	//생성자
+	public Goods(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//메소드 - gs
+	
+	//메소드 일반
 	
 	
 }
