c9457a622194dbb59366a471e421cb7762f93b44
최정필 <wjdvlf1407@gmail.com>
2022-04-22 19:20:30 +0900

    Ex01 멤버 프린트하기

--- src/com/javaex/ex01/MemberApp.java
+++ src/com/javaex/ex01/MemberApp.java
@@ -5,9 +5,9 @@ public class MemberApp {
 	public static void main(String[] args) {
 		
 		Member jws = new Member();	//정우성
-			jws.setName ("정우성");
-			jws.setId ("jws");
-			jws.setPoint (50000);
+		jws.setName ("정우성");
+		jws.setId ("jws");
+		jws.setPoint (50000);
 		
 		Member yjs = new Member();	//유재석
 		yjs.setName ("유재석");
@@ -15,14 +15,15 @@ public class MemberApp {
 		yjs.setPoint (30000);
 		
 		
-		
-		
 		Member lhr = new Member();	//이효리
+		lhr.setName ("이효리");
+		lhr.setId ("lhr");
+		lhr.setPoint (40000);
 		
 		
-		
-		
-		
+		System.out.println("회원정보: "+jws.getName()+"("+jws.getId()+"), "+jws.getPoint()+"점");	//정우성
+		System.out.println("회원정보: "+yjs.getName()+"("+yjs.getId()+"), "+yjs.getPoint()+"점");	//유재석
+		System.out.println("회원정보: "+lhr.getName()+"("+lhr.getId()+"), "+lhr.getPoint()+"점");	//이효리
 		
 	}
 
