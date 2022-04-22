7470919122fb6b6cd2e0a0664b5321df510ff7ae
최정필 <wjdvlf1407@gmail.com>
2022-04-22 23:32:14 +0900

    Ex04 주석정리

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -7,28 +7,26 @@ public class Base {
 	private String service;	
 	
 	//생성자
+	public void service(String state) {
+		//코드작성할 것
+		this.service=state;
+		if (service.equals("낮")) {		//tod 초기화
+			day();
+			
+		} else if(service.equals("밤")) {
+			night();
+			
+		} else {
+			afternoon();
+			
+		}
+		
+	}
 	
     //메소드 - gs
 	
 	
 	//메소드 - 일반
-	
-	
-    public void service(String state) {
-    	//코드작성할 것
-        this.service=state;
-        if (service.equals("낮")) {		//인스턴스 초기화
-        	day();
-			
-		} else if(service.equals("밤")) {
-			night();
-
-		} else {
-			afternoon();
-			
-		}
-			
-    }
 
     public void day() {
         System.out.println("낮에는 열심히 수업듣자");
