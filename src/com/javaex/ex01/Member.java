e7a5589d7653d2ffebd2afe5a9b28e9b907299fa
최정필 <wjdvlf1407@gmail.com>
2022-04-22 19:14:40 +0900

    Ex01 작성중

--- src/com/javaex/ex01/Member.java
+++ src/com/javaex/ex01/Member.java
@@ -1,5 +1,46 @@
 package com.javaex.ex01;
 
 public class Member {
+	
+	//필드
+	private String id;
+	private String name;
+	private int point;
+	
+	//생성자
+	
+	public Member () {
+		
+	}
+	
+	//메소드 - gs
+	//set
+	public void setId(String id) {
+		this.id=id;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public void setPoint(int point) {
+		this.point=point;
+	}
+	
+	//get
+	public String getId() {
+		return id;
+	}	
+	
+	public String getName() {
+		return name;
+		
+	}	
+	
+	public int getPoint() {
+		return point;
+	}	
+	
+	//메소드 - 일반
 
 }
--- src/com/javaex/ex01/MemberApp.java
+++ src/com/javaex/ex01/MemberApp.java
@@ -3,7 +3,26 @@ package com.javaex.ex01;
 public class MemberApp {
 
 	public static void main(String[] args) {
-
+		
+		Member jws = new Member();	//정우성
+			jws.setName ("정우성");
+			jws.setId ("jws");
+			jws.setPoint (50000);
+		
+		Member yjs = new Member();	//유재석
+		yjs.setName ("유재석");
+		yjs.setId ("yjs");
+		yjs.setPoint (30000);
+		
+		
+		
+		
+		Member lhr = new Member();	//이효리
+		
+		
+		
+		
+		
 		
 	}
 
