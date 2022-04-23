07782df1861a1c4b1ccf47424dd84ff33cb4a72a
최정필 <wjdvlf1407@gmail.com>
2022-04-23 19:44:22 +0900

    Ex10 작성중

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,6 +1,65 @@
 package com.javaex.ex10;
 
 public class Book {
+	
+	//필드
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+	
+	//생성자
+	public Book() {
+		
+	}
+	
+	public Book(int bookNo, String title, String author) {
+		this.bookNo=bookNo;
+		this.title=title;
+		this.author=author;
+		stateCode=1;
+	}
+
+	//메소드 -gs 
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	//메소드 - 일반
+	public void rent() {
+		stateCode=0;
+		System.out.print(this.title+"이()가 대여됐습니다.");
+	}
+	
+	public void print() {
+			System.out.println("대여중");
+			
+			System.out.println("재고있음");
+	}
+	public void displayBookInfo() {
+		System.out.println(bookNo+"책 제목: "+title+", 작가: "+author+", 대여 유무: ");
+		
+	}
     
     
     
--- src/com/javaex/ex10/BookShop.java
+++ src/com/javaex/ex10/BookShop.java
@@ -7,7 +7,7 @@ public class BookShop {
     public static void main(String[] args) {
         Book[] books = new Book[10];
 
-		/*books[0] = new Book(1, "트와일라잇", "스테파니메이어");
+		books[0] = new Book(1, "트와일라잇", "스테파니메이어");
 		books[1] = new Book(2, "뉴문", "스테파니메이어");
 		books[2] = new Book(3, "이클립스", "스테파니메이어");
 		books[3] = new Book(4, "브레이킹던", "스테파니메이어");
@@ -16,7 +16,7 @@ public class BookShop {
 		books[6] = new Book(7, "아프니깐 청춘이다", "김난도");
 		books[7] = new Book(8, "귀천", "천상병");
 		books[8] = new Book(9, "태백산맥", "조정래");
-		books[9] = new Book(10, "풀하우스", "원수연");*/
+		books[9] = new Book(10, "풀하우스", "원수연");
 
         System.out.println("*****도서 정보 출력하기******");
         displayBookInfo(books);
