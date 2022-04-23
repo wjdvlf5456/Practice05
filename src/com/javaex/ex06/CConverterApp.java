25fd8472207655f5e75b164ed18a5bc561bc2755
최정필 <wjdvlf1407@gmail.com>
2022-04-23 18:25:03 +0900

    Ex06 환율계산기

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -8,12 +8,19 @@ public class CConverterApp {
         double dollar;
         double won;
         
+        
         CConverter.setRate(1118.70);
         
         //100만원을 달러로 출력하기
-
+        won=1000000;
+        CConverter.toDoller(won);
+        System.out.println((int)won+"원은 "+CConverter.toDoller(won)+"원입니다.");
+        
         
         //100달려를 원으로 출력하기
+        dollar=100;
+        //CConverter의 toKWR에 들어갈 달러값을 설정해주고 dollar toKWR함수에 넣어주니 출력이 되었다.
+        System.out.println((int)dollar+"달러는 "+CConverter.toKWR(dollar)+"원입니다.");
         
         
     }
