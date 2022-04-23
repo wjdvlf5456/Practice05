c271d22683d473473d24aadd20873d96a8ba7f79
최정필 <wjdvlf1407@gmail.com>
2022-04-23 17:43:11 +0900

    Ex05 메소드 추가로 출력

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -1,11 +1,29 @@
 package com.javaex.ex05;
 
 public class Print {
-    
+	
+	//필드 기본으로 되어있음
+	
+	
+	
+	
     public void printer(int val){
         System.out.println(val);
     }
 
     //메소드  3개 를 작성하세요
+    public void printer(boolean yesorno) {
+    	System.out.println(yesorno);
+    	
+    }
+    public void printer(double val2) {
+    	System.out.println(val2);
+    	
+    }
+    public void printer(String name) {
+    	System.out.println(name);
+    	
+    }
+    
     
 }
--- src/com/javaex/ex05/PrintApp.java
+++ src/com/javaex/ex05/PrintApp.java
@@ -3,14 +3,15 @@ package com.javaex.ex05;
 public class PrintApp {
 
     public static void main(String[] args) {
-
-		/* Print print = new Print();
+    	
+    	//아래 틀은 건드리지 마시오.]]]]]]]]]
+		Print print = new Print();
 		
 		print.printer(10);
 		print.printer(true);
 		print.printer(5.7);
 		print.printer("홍길동");
-		*/
+		
     }
 
 }
