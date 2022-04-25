fe24286ae92d30781f370b81eb5e9db63cf02279
최정필 <wjdvlf1407@gmail.com>
2022-04-25 15:25:23 +0900

    Ex10 완료

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,26 +1,30 @@
 package com.javaex.ex10;
 
 public class Book {
-	
-	//필드
+
+	// 필드
 	private int bookNo;
 	private String title;
 	private String author;
 	private int stateCode;
-	
-	//생성자
+
+	// 생성자
 	public Book() {
-		
-	}
-	
-	public Book(int bookNo, String title, String author) {
-		this.bookNo=bookNo;
-		this.title=title;
-		this.author=author;
-		stateCode=1;
+
 	}
 
-	//메소드 -gs 
+	public Book(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		stateCode = 1;
+	}
+
+	// 메소드 -gs
 	public int getBookNo() {
 		return bookNo;
 	}
@@ -44,25 +48,22 @@ public class Book {
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-	
-	//메소드 - 일반
+
+	// 메소드 - 일반
 	public void rent() {
-		stateCode=0;
-		System.out.print(this.title+"이()가 대여됐습니다.");
+		stateCode = 0;
+		System.out.println(this.title + "이()가 대여됐습니다.");
 	}
-	
+
 	public void print() {
-			System.out.println("대여중");
-			
-			System.out.println("재고있음");
+		if (stateCode == 1) {
+			System.out.println(bookNo + " 책 제목: " + title + ", 작가: " + author + ", 대여 유무: 재고있음");
+
+		} else {
+			System.out.println(bookNo + " 책 제목: " + title + ", 작가: " + author + ", 대여 유무: 대여 중");
+
+		}
+
 	}
-	public void displayBookInfo() {
-		System.out.println(bookNo+"책 제목: "+title+", 작가: "+author+", 대여 유무: ");
-		
-	}
-    
-    
-    
-    
-    
+
 }
