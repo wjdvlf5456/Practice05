254b42cab90fb47712b489011639b57cfd66c1da
최정필 <wjdvlf1407@gmail.com>
2022-04-25 15:21:10 +0900

    Ex09 반복문 미사용

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -1,11 +1,16 @@
 package com.javaex.ex09;
 
 public class StringUtil {
-    
-	/*    public static String concatString(){
-	   
+
+
+	public static String concatString(String[] strArray){
 	    //메소드 내용작성
-	    
+		
+		String sum = strArray[0]+strArray[1]+strArray[2];
+		return sum;
+		
+		
 	}
-	*/
+
+
 }
--- src/com/javaex/ex09/StringUtilApp.java
+++ src/com/javaex/ex09/StringUtilApp.java
@@ -2,12 +2,13 @@ package com.javaex.ex09;
 
 public class StringUtilApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		/*String[] strArray = {"SuperMan", "BatMan", "SpiderMan"};
+		String[] strArray = { "SuperMan", "BatMan", "SpiderMan" };
 		String resultStr = StringUtil.concatString(strArray);
-		
-		System.out.println("결과 문자열:" + resultStr);*/
-    }
+
+		System.out.println("결과 문자열:" + resultStr);
+
+	}
 
 }
