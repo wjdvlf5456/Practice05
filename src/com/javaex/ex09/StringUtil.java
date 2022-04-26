efba4c81e0eaee0f0f00cd474838c673e631501e
최정필 <wjdvlf1407@gmail.com>
2022-04-26 13:38:19 +0900

    Ex09 for 사용

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,15 +2,15 @@ package com.javaex.ex09;
 
 public class StringUtil {
 
+	public static String concatString(String[] strArray) {
+		// 메소드 내용작성
+		String sum = "";
 
-	public static String concatString(String[] strArray){
-	    //메소드 내용작성
-		
-		String sum = strArray[0]+strArray[1]+strArray[2];
+		for (int i = 0; i < strArray.length; i++) {
+			sum = sum + strArray[i];
+		}
 		return sum;
-		
-		
-	}
 
+	}
 
 }
